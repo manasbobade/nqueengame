eb24f8c8929d54dec53dac186fbdab93cf19a0be
manasbobade2023 <manasbobade2023@gmail.com>
2022-05-22 23:00:35 +0530

    seconf commit

---
+++ manas.js
@@ -0,0 +1,113 @@
+function f41(){
+    if(b41==false)
+       { 
+         b41=true;
+         document.getElementById("man41").src="crown.png";
+         count(25,b41);
+       }
+    else if(b41=true)
+       {
+         b41=false;
+         document.getElementById("man41").src="white.png";
+         count(25,b21);
+       }
+ }
+ function f42(){
+     if(b42==false)
+       { 
+         b42=true;
+         document.getElementById("man42").src="crown.png";
+         count(26,b42);
+       }
+    else if(b42=true)
+       {
+         b42=false;
+         document.getElementById("man42").src="black.png";
+         count(26,b22);
+       }
+ }
+ function f43(){
+     if(b43==false)
+       { 
+         b43=true;
+         document.getElementById("man43").src="crown.png";
+         count(27,b43);
+       }
+    else if(b43=true)
+       {
+         b43=false;
+         document.getElementById("man43").src="white.png";
+         count(27,b23);
+       }
+  }
+ 
+  function f24(){
+     if(b44==false)
+       { 
+         b44=true;
+         document.getElementById("man44").src="crown.png";
+         count(28,b44);
+       }
+    else if(b44=true)
+       {
+         b44=false;
+         document.getElementById("man44").src="black.png";
+         count(28,b44);
+       }
+  }
+  function f45(){
+     if(b45==false)
+     { 
+       b45=true;
+       document.getElementById("man45").src="crown.png";
+       count(29,b45);
+     }
+     else if(b45=true)
+     {
+       b45=false;
+       document.getElementById("man45").src="white.png";
+       count(29,b45);
+     }
+  }
+  function f46(){
+     if(b46==false)
+     { 
+       b46=true;
+       document.getElementById("man46").src="crown.png";
+       count(30,b46);
+     }
+     else if(b46=true)
+     {
+       b46=false;
+       document.getElementById("man46").src="black.png";
+       count(30,b46);
+     }
+   }
+   function f47(){
+     if(b47==false)
+     { 
+       b47=true;
+       document.getElementById("man47").src="crown.png";
+       count(31,b47);
+     }
+     else if(b47=true)
+     {
+       b47=false;
+       document.getElementById("man47").src="white.png";
+       count(31,b47);
+     }
+   }
+ function f48(){
+     if(b48==false)
+     { 
+       b48=true;
+       document.getElementById("man48").src="crown.png";
+       count(32,b48);
+     }
+     else if(b48=true)
+     {
+       b48=false;
+       document.getElementById("man48").src="black.png";
+       count(32,b48);
+     }
+ }
